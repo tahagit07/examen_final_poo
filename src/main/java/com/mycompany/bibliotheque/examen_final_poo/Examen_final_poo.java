e428feed6921d8e665356bc1c63128e3d8c8a7e3
Taha Bennani <2417008@grasset.qc.ca>
2025-02-21 13:49:14 -0500

    first commit

---
+++ src/main/java/com/mycompany/bibliotheque/examen_final_poo/Examen_final_poo.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.bibliotheque.examen_final_poo;
+
+/**
+ *
+ * @author 2417008
+ */
+public class Examen_final_poo {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
